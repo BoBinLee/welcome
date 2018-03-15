a815f2ec528c8597da072346d7b6f5ce3a853722
bobinlee <cultist_tp@naver.com>
2018-03-15 14:54:17 +0900

    $set 속성 추가.

--- update/addons-update.js
+++ update/addons-update.js
@@ -20,8 +20,14 @@ ALL_COMMANDS_LIST.forEach(function (command) {
     ALL_COMMANDS_SET[command] = true;
 });
 
-const update = (value, spec) => {
-    const nextValue = { ...value };
+const update = (value, spec = {}) => {
+    let nextValue = { ...value };
+
+    if (spec.hasOwnProperty(COMMAND_SET)) {
+        nextValue = {
+            ...spec[COMMAND_SET]
+        };
+    }
     return nextValue;
 };
 
